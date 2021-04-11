d9c29cdf67c27cdb136664964065bb1e18348f3e
Stewart ilondanga <stilondanga@gmail.com>
2021-04-11 15:49:59 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -122,41 +122,41 @@ var navigate = (function() {
     function keyUp(e) {
       move(e, false);
     }
+
+    function keyDown(e) {
+      move(e, true);
+    }
     /*
-                        function keyDown(e) {
-                          move(e, true);
-                        }
+                            function move(e, isKeyDown) {
+                              if (e.keyCode >= 37 && e.keyCode <= 40) {
+                                e.preventDefault();
+                              }
 
-                        function move(e, isKeyDown) {
-                          if (e.keyCode >= 37 && e.keyCode <= 40) {
-                            e.preventDefault();
-                          }
+                              if (e.keyCode === 37) {
+                                state.keypress.left = isKeyDown;
+                              }
 
-                          if (e.keyCode === 37) {
-                            state.keypress.left = isKeyDown;
-                          }
+                              if (e.keyCode === 38) {
+                                state.keypress.up = isKeyDown;
+                              }
 
-                          if (e.keyCode === 38) {
-                            state.keypress.up = isKeyDown;
-                          }
+                              if (e.keyCode === 39) {
+                                state.keypress.right = isKeyDown;
+                              }
 
-                          if (e.keyCode === 39) {
-                            state.keypress.right = isKeyDown;
-                          }
+                              if (e.keyCode === 40) {
+                                state.keypress.down = isKeyDown;
+                              }
+                            }
 
-                          if (e.keyCode === 40) {
-                            state.keypress.down = isKeyDown;
-                          }
-                        }
+                            function camera(el) {
+                              document.documentElement.setAttribute('class', el.getAttribute('class'));
+                            }
 
-                        function camera(el) {
-                          document.documentElement.setAttribute('class', el.getAttribute('class'));
-                        }
+                            window.drive = {
+                              init: init,
+                              camera: camera
+                            }
+                          })();
 
-                        window.drive = {
-                          init: init,
-                          camera: camera
-                        }
-                      })();
-
-                      drive.init("car");
+                          drive.init("car");
