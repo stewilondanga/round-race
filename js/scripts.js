ca85fc56ff6ae85dbd83fd341602b29d5e59c3c9
Stewart ilondanga <stilondanga@gmail.com>
2021-04-11 15:45:06 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -57,106 +57,106 @@ var navigate = (function() {
         state.transformProperty = "webkitTransform";
       }
     }
-    /*
-                      function calcMovement() {
-                        if(state.keypress.up) {
-                          if(state.car.speed === 0) {
-                            state.car.speed = 0.1;
-                          }
 
-                          if(state.car.speed < state.car.model.maxSpeed) {
-                            state.car.speed += state.car.model.acceleration;
-                          }
-                        } else if(state.keypress.down && state.car.speed > state.car.model.maxReverseSpeed) {
-                          state.car.speed -= (state.car.model.acceleration / 2);
-                        }
+    function calcMovement() {
+      if (state.keypress.up) {
+        if (state.car.speed === 0) {
+          state.car.speed = 0.1;
+        }
+        /*
+                                  if(state.car.speed < state.car.model.maxSpeed) {
+                                    state.car.speed += state.car.model.acceleration;
+                                  }
+                                } else if(state.keypress.down && state.car.speed > state.car.model.maxReverseSpeed) {
+                                  state.car.speed -= (state.car.model.acceleration / 2);
+                                }
 
-                        if (state.keypress.left) {
-                          if(state.car.speed && state.car.turn < state.car.model.turnMax) {
-                            state.car.turn += 0.4;
-                          }
-                        } else if (state.keypress.right) {
-                          if(state.car.speed && state.car.turn > -state.car.model.turnMax) {
-                            state.car.turn -= 0.4;
-                          }
-                        }
+                                if (state.keypress.left) {
+                                  if(state.car.speed && state.car.turn < state.car.model.turnMax) {
+                                    state.car.turn += 0.4;
+                                  }
+                                } else if (state.keypress.right) {
+                                  if(state.car.speed && state.car.turn > -state.car.model.turnMax) {
+                                    state.car.turn -= 0.4;
+                                  }
+                                }
 
-                        state.car.speed *= state.car.model.friction;
+                                state.car.speed *= state.car.model.friction;
 
-                        if(state.car.speed < 0.1 && state.car.speed > -0.1) {
-                          state.car.speed = 0;
-                        }
+                                if(state.car.speed < 0.1 && state.car.speed > -0.1) {
+                                  state.car.speed = 0;
+                                }
 
-                        if(state.car.speed != 0 && state.car.turn != 0) {
-                          state.car.dir += state.car.turn * (state.car.speed / state.car.model.maxSpeed);
-                          state.car.turn *= 0.7
+                                if(state.car.speed != 0 && state.car.turn != 0) {
+                                  state.car.dir += state.car.turn * (state.car.speed / state.car.model.maxSpeed);
+                                  state.car.turn *= 0.7
 
-                          if(state.car.dir < 0) {
-                            state.car.dir = (360 - state.car.dir);
-                          } else if (state.car.dir > 360) {
-                            state.car.dir = 360 - state.car.dir;
-                          }
-                        }
+                                  if(state.car.dir < 0) {
+                                    state.car.dir = (360 - state.car.dir);
+                                  } else if (state.car.dir > 360) {
+                                    state.car.dir = 360 - state.car.dir;
+                                  }
+                                }
 
-                        if(state.car.turn < 0.1 && state.car.turn > -0.1) {
-                          state.car.turn = 0;
-                        }
+                                if(state.car.turn < 0.1 && state.car.turn > -0.1) {
+                                  state.car.turn = 0;
+                                }
 
-                        calcPosition();
-                      }
+                                calcPosition();
+                              }
 
-                      function calcPosition() {
-                        var dirRadian = state.car.dir * (Math.PI/180);
-                        state.car.x += state.car.speed * Math.sin(dirRadian);
-                        state.car.y += state.car.speed * Math.cos(dirRadian);
-                      }
+                              function calcPosition() {
+                                var dirRadian = state.car.dir * (Math.PI/180);
+                                state.car.x += state.car.speed * Math.sin(dirRadian);
+                                state.car.y += state.car.speed * Math.cos(dirRadian);
+                              }
 
-                      function animLoop() {
-                        setTimeout(function() {
-                          calcMovement();
-                          state.car.render(state.transformProperty);
-                          requestAnimationFrame(animLoop);
-                        }, 100 / state.fps);
-                      }
+                              function animLoop() {
+                                setTimeout(function() {
+                                  calcMovement();
+                                  state.car.render(state.transformProperty);
+                                  requestAnimationFrame(animLoop);
+                                }, 100 / state.fps);
+                              }
 
-                      function keyUp(e) {
-                        move(e, false);
-                      }
+                              function keyUp(e) {
+                                move(e, false);
+                              }
 
-                      function keyDown(e) {
-                        move(e, true);
-                      }
+                              function keyDown(e) {
+                                move(e, true);
+                              }
 
-                      function move(e, isKeyDown) {
-                        if(e.keyCode >= 37 && e.keyCode <= 40) {
-                          e.preventDefault();
-                        }
+                              function move(e, isKeyDown) {
+                                if(e.keyCode >= 37 && e.keyCode <= 40) {
+                                  e.preventDefault();
+                                }
 
-                        if(e.keyCode === 37) {
-                          state.keypress.left = isKeyDown;
-                        }
+                                if(e.keyCode === 37) {
+                                  state.keypress.left = isKeyDown;
+                                }
 
-                        if(e.keyCode === 38) {
-                          state.keypress.up = isKeyDown;
-                        }
+                                if(e.keyCode === 38) {
+                                  state.keypress.up = isKeyDown;
+                                }
 
-                        if(e.keyCode === 39) {
-                          state.keypress.right = isKeyDown;
-                        }
+                                if(e.keyCode === 39) {
+                                  state.keypress.right = isKeyDown;
+                                }
 
-                        if(e.keyCode === 40) {
-                          state.keypress.down = isKeyDown;
-                        }
-                      }
+                                if(e.keyCode === 40) {
+                                  state.keypress.down = isKeyDown;
+                                }
+                              }
 
-                      function camera(el) {
-                        document.documentElement.setAttribute('class', el.getAttribute('class'));
-                      }
+                              function camera(el) {
+                                document.documentElement.setAttribute('class', el.getAttribute('class'));
+                              }
 
-                      window.drive = {
-                    		init: init,
-                        camera: camera
-                    	}
-                    })();
+                              window.drive = {
+                            		init: init,
+                                camera: camera
+                            	}
+                            })();
 
-                    drive.init("car");
+                            drive.init("car");
